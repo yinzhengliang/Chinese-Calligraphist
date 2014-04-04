fa05dd9a8b818510e0e1981f7679986a5d91bad3
Zhengliang Yin <yinzhengliang@tamu.edu>
2014-04-03 19:01:07 -0500

    Day 3: Preprocessor
    4 methods:
    resample
    rotation
    scale
    and translate

---
+++ src/recognizer/Preprosessor.java
@@ -0,0 +1,22 @@
+package recognizer;
+
+import core.sketch.Stroke;
+
+public class Preprosessor {
+	public static void resample(Stroke stroke) {
+		
+	}
+	
+	public static void rotation(Stroke stroke) {
+		
+	}
+	
+	public static void scale(Stroke stroke, Double size) {
+		
+	}
+	
+	public static void translate(Stroke stroke) {
+		
+	}
+	
+}
